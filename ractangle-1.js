396fdb886b902120ccd56419640e0cbf5e9ee5a8
bpcrao <bpcrao@gmail.com>
2016-05-04 08:02:15 +0530

    intial version

---
+++ ractangle-1.js
@@ -0,0 +1,7 @@
+exports.perimeter = function(length, bredth){
+		return 2 *( length + bredth );
+};
+
+exports.area =  function(length, bredth){
+		return ( length * bredth );
+};
---
+++ solve-1.js
@@ -0,0 +1,10 @@
+var rect = require('./ractangle-1');
+
+function solveRectangle(l,b) { 
+	console.log("Area is ",rect.area(l,b));
+	console.log("Perimeter is ",rect.perimeter(l,b));
+}
+
+solveRectangle(5,9);
+solveRectangle(1,9);
+solveRectangle(5,2);
